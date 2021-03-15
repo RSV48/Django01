1eeeb764f3adba94243a26a64bad434ddfc45616
rsv <ena@enagent.ru>
2021-03-15 10:22:12 +0300

    Task Lesson 04

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authapp',
     'mainapp',
 ]
 
@@ -121,3 +122,5 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+AUTH_USER_MODEL = 'authapp.ShopUser'
