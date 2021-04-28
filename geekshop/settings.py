9c29edec6eca67474ebbe1b6f8f0e80b825611b7
rsv <ena@enagent.ru>
2021-04-28 16:09:43 +0300

    Настройка под сервер

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'o7bm)f5r-(+o@elhb+r*o-bmk2e16panwptwuo#r$$5bxf#vp9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = []
 
 # Application definition
 
