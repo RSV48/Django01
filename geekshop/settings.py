afbf2c8b74fbbb07372842ca722e9801e68c5ce7
rsv <ena@enagent.ru>
2021-05-04 12:25:13 +0300

    Добавлена ссылка на статику

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -160,10 +160,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    BASE_DIR / 'static',
+# статика для работы на сервере
+STATIC_ROOT = os.path.join(
+    BASE_DIR,'static'
 )
+
+# STATICFILES_DIRS = (
+#     BASE_DIR / 'static',
+# )
 
 MEDIA_URL = '/media/'
 
